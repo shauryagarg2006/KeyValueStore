37f1807593c8f483ca2cf6378887d1261653449f
Amit Barve <mtbar131@gmail.com>
2017-03-24 22:45:11 -0400

    added sample client code

---
+++ Client/src/main/java/edu/ncsu/client/Client.java
@@ -0,0 +1,24 @@
+package edu.ncsu.client;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+
+import edu.ncsu.store.ObjectServer;
+import edu.ncsu.store.StoreDriver;
+
+/**
+ * Created by amit on 24/3/17.
+ */
+public class Client {
+  public static void main(String args[]) {
+    try {
+      ArrayList<InetAddress> bootstrapNode = new ArrayList<>();
+      for (int i = 0; i < args.length; i++) {
+        bootstrapNode.add(InetAddress.getByName(args[i]));
+      }
+      StoreDriver driver = new StoreDriver(bootstrapNode, true);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
