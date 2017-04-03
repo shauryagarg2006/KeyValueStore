1c23a6fee52f4275970965d5f0294366671687a8
Shaurya Garg <shauryagarg2006@hotmail.com>
2017-04-03 02:37:51 -0400

    Key STore Test

--- Resources/docker_scripts/dKeyStoreTest.sh
+++ Resources/docker_scripts/dKeyStoreTest.sh
@@ -4,4 +4,4 @@
 sudo python ~/DockerImage/keyvalue/Resources/docker_scripts/FetchIPAddress.py
 
 # run maven tests
-sudo docker run -v ~/DockerImage/keyvalue:/root/KeyValueStore/ keyvalue bash -c "cd /root/KeyValueStore/Chord;mvn install -Dmaven.test.skip=true;cd /root/KeyValueStore/ObjectStore;mvn install -Dmaven.test.skip=true;cd /root/KeyValueStore/Client;mvn clean test"
+sudo docker run -v ~/DockerImage/keyvalue:/root/KeyValueStore/ keyvalue bash -c "cd /root/KeyValueStore/Chord;mvn install -Dmaven.test.skip=true;cd /root/KeyValueStore/ObjectStore;mvn install -Dmaven.test.skip=true;cd /root/KeyValueStore/Client;mvn exec:java"
