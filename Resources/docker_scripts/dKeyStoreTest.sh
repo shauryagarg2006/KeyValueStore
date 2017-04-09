ad16d60129f711ca1eef7b7b8b1dddab7be8544b
Amit Barve <mtbar131@gmail.com>
2017-04-09 14:17:23 -0400

    dont run PutKeys in KeyStoreTest

--- Resources/docker_scripts/dKeyStoreTest.sh
+++ Resources/docker_scripts/dKeyStoreTest.sh
@@ -6,5 +6,4 @@ set -x
 sudo python ~/DockerImage/keyvalue/Resources/docker_scripts/FetchIPAddress.py
 
 # run maven tests
-dPutKeys.sh
 sudo docker run -v ~/DockerImage/keyvalue:/root/KeyValueStore/ keyvalue bash -c "cd /root/KeyValueStore/Chord;mvn install -Dmaven.test.skip=true;cd /root/KeyValueStore/ObjectStore;mvn install -Dmaven.test.skip=true;cd /root/KeyValueStore/Client;mvn clean test"
