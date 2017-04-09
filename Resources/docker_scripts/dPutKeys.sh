8694f17580f5b34f167226524b84e24388ec016e
Amit Barve <mtbar131@gmail.com>
2017-04-08 22:45:24 -0400

    script maven compile before exec

--- Resources/docker_scripts/dPutKeys.sh
+++ Resources/docker_scripts/dPutKeys.sh
@@ -3,4 +3,4 @@
 set -x
 
 # run maven tests
-sudo docker run -v ~/DockerImage/keyvalue:/root/KeyValueStore/ keyvalue bash -c "cd /root/KeyValueStore/Chord;mvn install -Dmaven.test.skip=true;cd /root/KeyValueStore/ObjectStore;mvn install -Dmaven.test.skip=true;cd /root/KeyValueStore/Client;mvn clean exec:java"
+sudo docker run -v ~/DockerImage/keyvalue:/root/KeyValueStore/ keyvalue bash -c "cd /root/KeyValueStore/Chord;mvn install -Dmaven.test.skip=true;cd /root/KeyValueStore/ObjectStore;mvn install -Dmaven.test.skip=true;cd /root/KeyValueStore/Client;mvn compile exec:java"
