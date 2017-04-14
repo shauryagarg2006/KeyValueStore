f409d90a9bcddc86f74a739faef527ff86f9ce7d
Amit Barve <mtbar131@gmail.com>
2017-04-13 20:22:47 -0400

    Hashamp size optimization

--- ObjectStore/src/main/java/edu/ncsu/store/HashStorage.java
+++ ObjectStore/src/main/java/edu/ncsu/store/HashStorage.java
@@ -13,6 +13,11 @@ class HashStorage implements LocalStorage {
 
   private HashMap<String, DataContainer> storage;
 
+  /* Having a default size of 1000 for the hashmap improves performance of get/put keys.
+  This might be because JVM by default creates HashMap of smaller size and then grows the value as more
+  keys come.However growing hashmap is a costly operation.
+   */
+  private static long HASHMAP_DEFAULT_SIZE = 1024;
   /* Keep all loggers transient so that they are not passed over RMI call */
   private final transient static Logger logger = Logger.getLogger(HashStorage.class);
 
