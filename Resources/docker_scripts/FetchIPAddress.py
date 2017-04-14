0e9bf8cabb3782ec911c1a09f6890adb0868f9c9
Amit Barve <mtbar131@gmail.com>
2017-04-14 19:47:54 -0400

    typo fixed

--- Resources/docker_scripts/FetchIPAddress.py
+++ Resources/docker_scripts/FetchIPAddress.py
@@ -6,7 +6,7 @@ from os.path import expanduser
 client = docker.APIClient(base_url='unix://var/run/docker.sock')
 container_list = client.containers()
 home = expanduser("~")
-target = open(home + "/DockerImage/keyalue/Resources/tests/node_ip_list", 'w')
+target = open(home + "/DockerImage/keyvalue/Resources/tests/node_ip_list", 'w')
 for container_id in container_list:
         target.write(container_id['NetworkSettings']['Networks']['bridge']['IPAddress'])
         target.write("\n")
