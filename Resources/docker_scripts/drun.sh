54cbb661ef3ebb48d6f710104f562b2c09a60b44
Amit Barve <mtbar131@gmail.com>
2017-04-14 20:47:17 -0400

    run only once

--- Resources/docker_scripts/drun.sh
+++ Resources/docker_scripts/drun.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
-for i in `seq 1 $1`
+sudo docker run -v ~/DockerImage/keyvalue:/root/KeyValueStore/ keyvalue &
+sleep 10
+for i in `seq 2 $1`
 do
     echo "$i"
     sudo docker run -v ~/DockerImage/keyvalue:/root/KeyValueStore/ keyvalue &
